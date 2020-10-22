d4994ce2ba192f4d88cdd2ac0ec1e8c8e0899be9
zatoulanypes <av_leontyev@mail.ru>
2020-10-22 17:57:19 +0300

    --

--- OneIndexedList.py
+++
@@ -1,9 +0,0 @@
-class OneIndexedList():
-    def __init__(self, items):
-        self.items = list(items)
-
-    def __getitem__(self, index):
-        return self.items[index-1]
-
-    def __setitem__(self, index, value):
-        self.items[index-1] = value
